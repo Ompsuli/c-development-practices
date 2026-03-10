44b5b69243471c05ec5b1b4d236474bcc01ed773
ompsuli <jaakko.pihlajamaa@gmail.com>
2026-03-10 17:33:49 +0200

    Add: More conditionals

--- beginner/operators/conditionals.c
+++ beginner/operators/conditionals.c
@@ -24,6 +24,20 @@ int main() {
 
   if(i >= j)
     printf("i is equal or greater than j\n");
+  
+  if(i > 5 && j > 10) {
+    printf("i is greater than 5 AND j is greater than 10\n");
+  }
+  else {
+    printf("i is not greater than 5 OR/AND j is not greater than 10\n");
+  };
+
+  if(!(i > 5) || !(j > 10)) {
+    printf("i is NOT greater than 5 OR j is NOT greater than 10\n");
+  }
+  else {
+    printf("i is greater than 5 OR j is greater than 10\n");
+  };
 
   return 0;
 }
