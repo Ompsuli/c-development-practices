bc85b359a7ac60860a6bfa26f72a8dfd77610b7e
Ompsuli <jaakko.pihlajamaa@gmail.com>
2026-03-26 09:26:56 +0200

    Edit: Signed & Unsigned types

--- beginner/dtypes/dtypes.c
+++ beginner/dtypes/dtypes.c
@@ -19,6 +19,7 @@ int main() {
   //Character
   char nickname = 'O';
   printf("My Github nickname starts with letter %c\n", nickname);
+  printf("My Github nickname int value is %d\n", nickname);
 
   //String == Char array
   char name[] = "Ompsuli";
@@ -29,6 +30,23 @@ int main() {
   bool cIsEasy = false;
   printf("Do I like C language? %d\n", likeC);
   printf("Do I think C is easy? %d\n", cIsEasy);
+  /* All above are signed dtypes */
+
+  //Unsigned values cannot be negatives. Signed values can be negative and positive.
+  signed int i = 999;
+  printf("Signed int: %d\n", i);
+  unsigned int u_i = 999;
+  printf("Signed unsigned int: %d\n", u_i);
+  short s_i = -99; // signed
+  printf("Manual signed int: %d\n", s_i);
+  long l_i = 999; // signed
+  printf("Long int: %ld\n", l_i);
+  long long ll_i = 9999; //signed
+  printf("Long long int: %lld\n", ll_i);
+  double d_i = 1.234567;
+  printf("double int: %f\n", d_i);
+  long double ld_i = 0.0012345;
+  printf("Long double int: %Lf\n", ld_i); //Show only 5 meaningful digits
 
   return 0;
 }
